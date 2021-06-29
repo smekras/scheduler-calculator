2662f42351f0eb2a8e739c8692153091664dfcf1
Stergios Mekras <stergios.mekras@gmail.com>
2021-06-29 13:12:09 +0200

    Fix temp queue

--- robin.py
+++ robin.py
@@ -31,6 +31,8 @@ class RoundRobin:
                     self.process_queue.pop(0)
             if len(temp_queue) > 0:
                 print("Temp:", temp_queue[0].name)
+            else:
+                print("Temp: None")
 
             if self.active == "":
                 print("Active: None")
@@ -44,6 +46,8 @@ class RoundRobin:
             else:
                 print("Active:", end=" ")
                 self.active.print_info()
+                if len(temp_queue) > 0:
+                    self.wait_queue.append(temp_queue[0])
 
             if self.active != "":
                 if self.active.start == "Unset":
@@ -62,7 +66,7 @@ class RoundRobin:
                     self.active.empty += 1
                     self.active.segments += 1
                     self.wait_queue.append(self.active)
-                    self.active = ""
+                    self.active = self.wait_queue[0]
 
             print("Wait Queue:", end=" ")
             utils.print_array_names(self.wait_queue)
@@ -76,7 +80,6 @@ class RoundRobin:
 
     def check_done(self):
         if self.active != "" and self.active != []:
-            self.active.print_info()
             remaining = self.active.remain
         else:
             remaining = 0
@@ -84,58 +87,13 @@ class RoundRobin:
         for p in self.process_queue:
             remaining += p.remain
 
+        if self.active != "":
+            print("Active:", end=" ")
+            self.active.print_info()
+        else:
+            print("Active: None")
         print("Total remaining:", remaining)
 
         if remaining == 0 or self.current_time == 20:
             self.done = 1
         self.current_time += 1
-
-    # def load_process(self, process):
-    #     if process.arrival == self.current_time:
-    #         self.load_to_wait(process)
-    #
-    # def load_to_active(self, process):
-    #     if self.active == "---" or "":
-    #         self.active = process.name
-    #         self.calculate_start(process)
-    #         print(self.wait_queue)
-    #         self.wait_queue.pop(0)
-    #         print(self.wait_queue)
-
-    # def calculate_start(self, process):
-    #     if 0 < process.remain <= self.quantum:
-    #         process.last = self.current_time
-    #     else:
-    #         process.start = self.current_time
-    #
-    # def calculate_turnaround(self, process):
-    #     process.turnaround = process.end - process.arrival
-    #
-    # def calculate_wait(self, process):
-    #     # way 1
-    #     wait1 = process.turnaround - process.burst
-    #     # way 2
-    #     wait2 = process.last - process.arrival - (process.empty * self.quantum)
-    #     if wait1 == wait2:
-    #         process.wait = wait1
-    #     else:
-    #         process.wait = "error"
-    #
-    # def suspend_process(self, process):
-    #     if process.start == self.current_time - 2 and process.remain > 0:
-    #         if process.remain <= 2:
-    #             process.remain = 0
-    #         else:
-    #             process.remain += -2
-    #         process.empty += 1
-    #         process.segments += 1
-    #         self.final_queue.append(self.active)
-    #         self.load_to_wait(process)
-
-    # def update_processes(self):
-    #     for key in self.processes:
-    #         print("*", key, self.processes[key])
-    #         self.calculate_start(self.processes[key])
-    #         self.calculate_turnaround(self.processes[key])
-    #         self.calculate_wait(self.processes[key])
-    #         print("-", key, self.processes[key])
--- temp.py
+++ temp.py
@@ -1,24 +1,51 @@
-# if arrival == current_time:
-#     temp_queue.append()
+processes = [["P1", 1, 3], ["P2", 3, 7], ["P3", 5, 1]]
+control = ["--", "P1", "P1", "P2", "P2", "P1", "P3", "P2", "P2", "P2", "P2", "P2"]
 
-# if active is empty and wait_queue not empty:
-#     load_to_active(wait_queue[0])
-#     if active.start == "Unset":
-#         active.start = current_time
-#     else:
-#         active.last = current_time
-#     final_queue.append(active)
+active = []
+turn_queue = []
+wait_queue = []
+final_queue = []
 
-# active.remain -= 1
+current_time = 0
 
-# if active.remain == 0 or active.last == current_time - quantum:
-#     if active.remain is not 0:
-#         active.empty += 1
-#         active.segments += 1
-#         wait_queue.append(active)
-#     else:
-#         active.last = active.start
-#     active = ""
+done = False
 
-# for _ in temp_queue:
-#     wait_queue.append(_)
+while not done:
+    print("\nTime:", current_time)
+    for p in processes:
+        if p[1] == current_time:
+            turn_queue.append(p)
+
+    print("Turn:", turn_queue)
+
+    if not active:
+        if len(turn_queue) > 0:
+            active = turn_queue[0]
+            turn_queue.pop(0)
+        else:
+            if len(wait_queue) > 0:
+                active = wait_queue[0]
+
+    if not turn_queue and not wait_queue:
+        active = ["--", 0, 0]
+
+    if active != [] and active[2] > 0:
+        wait_queue.append(active)
+
+    print("Active:", active)
+
+    if len(turn_queue) > 0:
+        wait_queue.append(turn_queue[0])
+
+    print("Wait:", wait_queue)
+
+    current_time += 1
+
+    if current_time == 13:
+        done = True
+
+if done:
+    if final_queue == control:
+        print("Success:", final_queue)
+    else:
+        print("Failure:", final_queue)
