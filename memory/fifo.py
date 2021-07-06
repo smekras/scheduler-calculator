8ab34655db5ef5de8658c8500f004d341f9ebe8d
Stergios Mekras <stergios.mekras@gmail.com>
2021-07-06 22:09:45 +0200

    Add final FIFO version

--- memory/fifo.py
+++ memory/fifo.py
@@ -1,39 +1,39 @@
-import time
-
 pages = [6, 5, 8, 7, 8, 2, 1, 4, 1, 4, 8, 2, 4, 5, 4, 7, 3, 4, 7, 3]
 
 frame = ["-", "-", "-"]
-hits = []
+success = []
 virtual = []
 
 frames = 3
 clock = 0
 first = 0
 last = 0
+faults = 0
+hits = 0
 done = 0
 
 while not done:
-    time.sleep(1)
     for page in pages:
+        clock += 1
 
         if page in frame:
-            hits.append(True)
-            print(clock, "Frame:", frame, "-", hits[clock])
+            success.append(True)
+            hits += 1
+            print(clock, "Frame:", frame, "-", success[clock - 1])
 
         if page not in frame:
-            frame[last] = page
-            last = frame.index(page)
-            if last == 2:
-                last = 0
-            else:
-                last += 1
-            hits.append(False)
-            print(clock, "Frame:", frame, "-", hits[clock])
+            frame.insert(0, page)
+            if len(frame) > 2:
+                frame.pop(3)
+            success.append(False)
+            faults += 1
+            print(clock, "Frame:", frame, "-", success[clock - 1])
 
         virtual.append(page)
-        clock += 1
 
     if virtual == pages:
         done = 1
 
-print("\nVirtual:", virtual)
+print("\nSuccess rate:", hits / clock)
+print("Faults:", faults)
+print("Hits:", hits)
